24cc60c44e68a4f5d323e3abd8d51d587ba5f69d
Gh0stF1ghter <91743885+Gh0stF1ghter@users.noreply.github.com>
2024-06-05 21:59:53 +0300

    feat: add interfaces for repos

---
+++ DAL/Repositories/Interfaces/IVehicleClientHistoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities;
+using System.Linq.Expressions;
+
+namespace DAL.Repositories.Interfaces;
+public interface IVehicleClientHistoryRepository
+{
+    public Task<IEnumerable<VehicleClientHistory>> GetVehicleClientHistoriesRangeAsync(int page, int pageSize, CancellationToken cancellationToken);
+    public Task<VehicleClientHistory?> GetVehicleClientHistoryByIdAsync(int id, CancellationToken cancellationToken);
+
+    public Task<bool> IsVehicleClientHistoryExists(Expression<Func<Client, bool>> predicate, CancellationToken cancellationToken);
+
+    public void AddVehicleClientHistory(VehicleClientHistory vehicleClientHistory);
+    public void RemoveVehicleClientHistory(VehicleClientHistory vehicleClientHistory);
+}

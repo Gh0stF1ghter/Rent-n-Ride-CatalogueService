8cd4a9b3e17771b051256f149dba09d1d8299e06
Gh0stF1ghter <91743885+Gh0stF1ghter@users.noreply.github.com>
2024-06-05 20:52:11 +0300

    feat: add context

---
+++ DAL/Context/AgencyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context;
+public class AgencyDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Manufacturer> Manufacturers { get; set; }
+    public DbSet<Model> Models { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<VehicleClientHistory> VehicleClientHistories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}

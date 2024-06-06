f129a8bbc126b2a6e773aede84da991c52c4edfe
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-06 11:23:46 +0300

    feat: implement VCH repo logic

--- DAL/Repositories/Implementations/VehicleClientHistoryRepository.cs
+++ DAL/Repositories/Implementations/VehicleClientHistoryRepository.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace DAL.Repositories.Implementations;
-public class VehicleClientHistoryRepository
+public class VehicleClientHistoryRepository(AgencyDbContext context) : IVehicleClientHistoryRepository
 {
+    public async Task<IEnumerable<VehicleClientHistory>> GetVehicleClientHistoriesRangeAsync(int page, int pageSize, CancellationToken cancellationToken)
+    {
+        var rowsToSkip = page - 1 * pageSize;
+
+        return await context.VehicleClientHistories
+            .Skip(rowsToSkip)
+            .Take(pageSize)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<VehicleClientHistory?> GetVehicleClientHistoryByIdAsync(int id, CancellationToken cancellationToken) =>
+        await context.VehicleClientHistories.FindAsync([id], cancellationToken);
+
+    public async Task<bool> IsVehicleClientHistoryExists(Expression<Func<VehicleClientHistory, bool>> predicate, CancellationToken cancellationToken) =>
+        await context.VehicleClientHistories.AnyAsync(predicate, cancellationToken);
+
+    public void AddVehicleClientHistory(VehicleClientHistory vehicleClientHistory) =>
+        context.VehicleClientHistories.Add(vehicleClientHistory);
+    public void RemoveVehicleClientHistory(VehicleClientHistory vehicleClientHistory) =>
+        context.VehicleClientHistories.Remove(vehicleClientHistory);
+
 }

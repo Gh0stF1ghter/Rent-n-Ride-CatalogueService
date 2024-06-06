3a787c36e996b1bcbe4613dab22dd47b01011c2a
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-06 11:23:55 +0300

    feat: implement vehicle repo logic

--- DAL/Repositories/Implementations/VehicleRepository.cs
+++ DAL/Repositories/Implementations/VehicleRepository.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace DAL.Repositories.Implementations;
-public class VehicleRepository
+public class VehicleRepository(AgencyDbContext context) : IVehicleRepository
 {
+    public async Task<IEnumerable<Vehicle>> GetVehiclesRangeAsync(int page, int pageSize, CancellationToken cancellationToken)
+    {
+        var rowsToSkip = page - 1 * pageSize;
+
+        return await context.Vehicles
+            .Skip(rowsToSkip)
+            .Take(pageSize)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+    public async Task<Vehicle?> GetVehicleByIdAsync(int id, CancellationToken cancellationToken) => 
+        await context.Vehicles.FindAsync([id], cancellationToken);
+
+    public Task<bool> IsVehicleExists(Expression<Func<Vehicle, bool>> predicate, CancellationToken cancellationToken) =>
+        context.Vehicles.AnyAsync(predicate, cancellationToken);
+
+    public void AddVehicle(Vehicle vehicle) =>
+        context.Vehicles.Add(vehicle);
+
+    public void RemoveVehicle(Vehicle vehicle) => 
+        context.Vehicles.Remove(vehicle);
 }

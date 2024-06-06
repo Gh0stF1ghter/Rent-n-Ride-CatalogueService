fc65b6010306d1ec09013fe4fcc023c844fabc51
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-06 11:22:52 +0300

    feat: implement model repo logic

--- DAL/Repositories/Implementations/ModelRepository.cs
+++ DAL/Repositories/Implementations/ModelRepository.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace DAL.Repositories.Implementations;
-public class ModelRepository
+public class ModelRepository(AgencyDbContext context) : IModelRepository
 {
+    public async Task<IEnumerable<Model>> GetModelsRangeAsync(int page, int pageSize, CancellationToken cancellationToken)
+    {
+        var rowsToSkip = page - 1 * pageSize;
+
+        return await context.Models.Skip(rowsToSkip).Take(pageSize).AsNoTracking().ToListAsync(cancellationToken);
+    }
+    public async Task<Model?> GetModelByIdAsync(int id, CancellationToken cancellationToken) =>
+        await context.Models.FindAsync([id], cancellationToken);
+
+    public async Task<bool> IsModelExists(Expression<Func<Model, bool>> predicate, CancellationToken cancellationToken) =>
+        await context.Models.AnyAsync(predicate, cancellationToken);
+
+    public void AddModel(Model model) =>
+        context.Models.Add(model);
+    public void RemoveModel(Model model) =>
+        context.Models.Remove(model);
+
 }

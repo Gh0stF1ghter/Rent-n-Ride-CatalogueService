c5eeed35a81483d236d29f5cb30df95b3d255b4d
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-06 11:21:50 +0300

    feat: implement manufacturer repo logic

--- DAL/Repositories/Implementations/ManufacturerRepository.cs
+++ DAL/Repositories/Implementations/ManufacturerRepository.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace DAL.Repositories.Implementations;
-public class ManufacturerRepository
+public class ManufacturerRepository(AgencyDbContext context) : IManufacturerRepository
 {
+    public async Task<IEnumerable<Manufacturer>> GetManufacturersRangeAsync(int page, int pageSize, CancellationToken cancellationToken)
+    {
+        var rowsToSkip = page - 1 * pageSize;
+
+        return await context.Manufacturers
+            .Skip(rowsToSkip)
+            .Take(pageSize)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<Manufacturer?> GetManufacturerByIdAsync(int id, CancellationToken cancellationToken) =>
+        await context.Manufacturers.FindAsync([id], cancellationToken);
+
+    public async Task<bool> IsManufacturerExists(Expression<Func<Manufacturer, bool>> predicate, CancellationToken cancellationToken) =>
+        await context.Manufacturers.AnyAsync(predicate, cancellationToken: cancellationToken);
+
+    public void AddManufacturer(Manufacturer manufacturer) =>
+        context.Manufacturers.Add(manufacturer);
+    public void RemoveManufacturer(Manufacturer manufacturer) =>
+        context.Manufacturers.Remove(manufacturer);
+
 }

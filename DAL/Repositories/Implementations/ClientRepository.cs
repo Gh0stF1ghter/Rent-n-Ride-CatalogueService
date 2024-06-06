930d1a5cc80222e78653e2bb16b900d3550c382f
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-06 11:22:01 +0300

    feat: implement client repo logic

--- DAL/Repositories/Implementations/ClientRepository.cs
+++ DAL/Repositories/Implementations/ClientRepository.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
 
 namespace DAL.Repositories.Implementations;
-public class ClientRepository
+public class ClientRepository(AgencyDbContext context) : IClientRepository
 {
+    public async Task<IEnumerable<Client>> GetClientsRangeAsync(int page, int pageSize, CancellationToken cancellationToken)
+    { 
+        var rowsToSkip = page - 1 * pageSize;
+
+        return await context.Clients
+            .Skip(rowsToSkip)
+            .Take(pageSize)
+            .Include(c => c.Vehicle).AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<Client?> GetClientByIdAsync(int id, CancellationToken cancellationToken) =>
+        await context.Clients.FindAsync([id], cancellationToken: cancellationToken);
+
+    public async Task<bool> IsClientExists(Expression<Func<Client, bool>> predicate, CancellationToken cancellationToken) =>
+        await context.Clients.AnyAsync(predicate, cancellationToken: cancellationToken);
+
+    public void AddClient(Client client) =>
+        context.Clients.Add(client);
+
+    public void RemoveClient(Client client) =>
+        context.Clients.Remove(client);
 }

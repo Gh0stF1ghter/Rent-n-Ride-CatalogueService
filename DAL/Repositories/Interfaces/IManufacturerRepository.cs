dba4462169196644c05dca903fae9172c5f3b055
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-07 11:40:59 +0300

    feat: add Update method to repos

--- DAL/Repositories/Interfaces/IManufacturerRepository.cs
+++ DAL/Repositories/Interfaces/IManufacturerRepository.cs
@@ -7,11 +7,13 @@ public interface IManufacturerRepository
 {
     Task<IEnumerable<Manufacturer>> GetRangeAsync(int page, int pageSize, CancellationToken cancellationToken);
 
-    Task<Manufacturer?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<Manufacturer?> GetByIdAsync(Guid id, bool trackingChanges, CancellationToken cancellationToken);
 
     Task<bool> IsExistsAsync(Expression<Func<Manufacturer, bool>> predicate, CancellationToken cancellationToken);
 
     Task AddAsync(Manufacturer manufacturer, CancellationToken cancellationToken);
+
+    Task UpdateAsync(Manufacturer newManufacturer, CancellationToken cancellationToken);
 
     Task RemoveAsync(Manufacturer manufacturer, CancellationToken cancellationToken);
 }

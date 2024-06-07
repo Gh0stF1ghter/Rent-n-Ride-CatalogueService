fd245babcdda2aae1f88918ac5a8bbe60eed4898
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-07 11:10:07 +0300

    feat: create service interfaces

---
+++ BLL/Services/Interfaces/IModelNameService.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services.Interfaces;
+
+internal interface IModelNameService
+{
+    Task<IEnumerable<ModelNameModel>> GetRangeAsync(int page, int pageSize, CancellationToken cancellationToken);
+
+    Task<ModelNameModel> GetByIdAsync(int id, CancellationToken cancellationToken);
+
+    Task<ModelNameModel> AddAsync(ModelNameModel modelNameModel, CancellationToken cancellationToken);
+
+    Task UpdateAsync(int id, ModelNameModel newModelNameModel);
+
+    Task DeleteAsync(int id);
+}
---
+++ BLL/Services/Interfaces/IVehicleClientHistoryService.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Entities;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services.Interfaces;
+internal interface IVehicleClientHistoryService
+{
+    Task<IEnumerable<VchModel>> GetRangeAsync(int page, int pageSize, CancellationToken cancellationToken);
+
+    Task<VchModel> GetByIdAsync(int id, CancellationToken cancellationToken);
+
+    Task<VchModel> AddAsync(VchModel vchModel, CancellationToken cancellationToken);
+
+    Task UpdateAsync(int id, VchModel newVchModel);
+
+    Task DeleteAsync(int id);
+}

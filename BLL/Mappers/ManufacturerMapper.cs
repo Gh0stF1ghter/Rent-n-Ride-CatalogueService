f3a86d19cc652570cbe3f1e574edb4cee6a6a52e
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-07 13:21:19 +0300

    Feature/adjust repos (#39)
    * refactor: rename model entity for more readability
    
    * feat: add Update method to repos
    
    * feat: implement Update method to repos
    
    * fix: minor property renaming

--- BLL/Mappers/ManufacturerMapper.cs
+++ BLL/Mappers/ManufacturerMapper.cs
@@ -1,4 +1,4 @@
-﻿using DAL.Entities;
+using DAL.Entities;
 using DAL.Models;
 
 namespace DAL.Mappers;
@@ -10,7 +10,7 @@ internal static class ManufacturerMapper
         {
             Id = manufacturerModel.Id,
             Name = manufacturerModel.Name,
-            ModelNames = manufacturerModel.Models,
+            ModelNames = manufacturerModel.ModelNames,
         };
 
     public static ManufacturerModel Map(Manufacturer manufacturer) =>
--- DAL/Repositories/Interfaces/IVehicleRepository.cs
+++ DAL/Repositories/Interfaces/IVehicleRepository.cs
@@ -7,11 +7,13 @@ public interface IVehicleRepository
 {
     Task<IEnumerable<Vehicle>> GetRangeAsync(int page, int pageSize, CancellationToken cancellationToken);
 
-    Task<Vehicle?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<Vehicle?> GetByIdAsync(Guid id, bool trackingChanges, CancellationToken cancellationToken);
 
     Task<bool> IsExistsAsync(Expression<Func<Vehicle, bool>> predicate, CancellationToken cancellationToken);
 
     Task AddAsync(Vehicle vehicle, CancellationToken cancellationToken);
+
+    Task UpdateAsync(Vehicle newVehicle, CancellationToken cancellationToken);
 
     Task RemoveAsync(Vehicle vehicle, CancellationToken cancellationToken);
 }

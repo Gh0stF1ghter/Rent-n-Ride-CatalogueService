16ece302acd6d7c8a986e2880b8d3a0052f7ff15
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-10 11:10:24 +0300

    feat: implement Manufacturer controller logic

---
+++ API/Controllers/ManufacturerController.cs
@@ -0,0 +1,66 @@
+﻿using BLL.Services.Interfaces;
+using BLL.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/manufacturer")]
+public class ManufacturerController(IManufacturerService service) : ControllerBase
+{
+    [HttpGet]
+    [ActionName("GetAllManufacturersInRange")]
+    [ProducesResponseType(typeof(IEnumerable<ManufacturerViewModel>), 200)]
+    public async Task<IActionResult> GetAll([FromQuery] int page, [FromQuery] int pageSize, CancellationToken cancellationToken)
+    {
+        var manufacturers = await service.GetRangeAsync(page, pageSize, cancellationToken);
+
+        return Ok(manufacturers);
+    }
+
+    [HttpGet("{id}")]
+    [ActionName("GetManufacturerById")]
+    [ProducesResponseType(typeof(ManufacturerViewModel), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    public async Task<IActionResult> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var manufacturer = await service.GetByIdAsync(id, cancellationToken);
+
+        return Ok(manufacturer);
+    }
+
+    [HttpPost]
+    [ActionName("CreateManufacturer")]
+    [ProducesResponseType(typeof(ManufacturerViewModel), (int)HttpStatusCode.Created)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> Create([FromBody] CreateManufacturerViewModel createManufacturerViewModel, CancellationToken cancellationToken)
+    {
+        var newManufacturer = await service.AddAsync(createManufacturerViewModel, cancellationToken);
+
+        return CreatedAtAction("GetManufacturerById", new { id = newManufacturer.Id }, newManufacturer);
+    }
+
+    [HttpPut("{id}")]
+    [ActionName("UpdateManufacturerById")]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] CreateManufacturerViewModel updateManufacturerViewModel, CancellationToken cancellationToken)
+    {
+        await service.UpdateAsync(id, updateManufacturerViewModel, cancellationToken);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    [ActionName("DeleteManufacturerById")]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    public async Task<IActionResult> Delete([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        await service.DeleteAsync(id, cancellationToken);
+
+        return NoContent();
+    }
+}

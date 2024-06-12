e790b22a3d5957ec8fc942b8c52b97eaeb5beb63
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-12 11:40:03 +0300

    feat: adjust specific repos to inherit generic logic

--- DAL/Repositories/Interfaces/IManufacturerRepository.cs
+++ DAL/Repositories/Interfaces/IManufacturerRepository.cs
@@ -3,17 +3,9 @@ using System.Linq.Expressions;
 
 namespace DAL.Repositories.Interfaces;
 
-public interface IManufacturerRepository
+public interface IManufacturerRepository : IRepositoryBase<Manufacturer>
 {
     Task<IEnumerable<Manufacturer>> GetRangeAsync(int page, int pageSize, CancellationToken cancellationToken);
 
     Task<Manufacturer?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
-
-    Task<bool> IsExistsAsync(Expression<Func<Manufacturer, bool>> predicate, CancellationToken cancellationToken);
-
-    Task AddAsync(Manufacturer manufacturer, CancellationToken cancellationToken);
-
-    Task UpdateAsync(Manufacturer newManufacturer, CancellationToken cancellationToken);
-
-    Task RemoveAsync(Manufacturer manufacturer, CancellationToken cancellationToken);
 }
--- DAL/Repositories/Interfaces/IModelNameRepository.cs
+++ DAL/Repositories/Interfaces/IModelNameRepository.cs
@@ -3,17 +3,9 @@ using System.Linq.Expressions;
 
 namespace DAL.Repositories.Interfaces;
 
-public interface IModelNameRepository
+public interface IModelNameRepository : IRepositoryBase<ModelName>
 {
     Task<IEnumerable<ModelName>> GetRangeAsync(int page, int pageSize, CancellationToken cancellationToken);
 
     Task<ModelName?> GetByIdAsync(Guid id, bool trackingChanges, CancellationToken cancellationToken);
-
-    Task<bool> IsExistsAsync(Expression<Func<ModelName, bool>> predicate, CancellationToken cancellationToken);
-
-    Task AddAsync(ModelName modelName, CancellationToken cancellationToken);
-
-    Task UpdateAsync(ModelName newModelName, CancellationToken cancellationToken);
-
-    Task RemoveAsync(ModelName modelName, CancellationToken cancellationToken);
 }

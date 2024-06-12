829e411e43c44e8e10163ff5eda2ab6287d3a361
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-12 16:11:58 +0300

    feat: implement repos mocks

---
+++ Tests/Mocks/VehicleRepositoryMock.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using Moq;
+using System.Linq.Expressions;
+
+namespace Tests.Mocks;
+
+internal class VehicleRepositoryMock : Mock<IVehicleRepository>
+{
+    private readonly CancellationToken _anyToken = It.IsAny<CancellationToken>();
+
+    public void GetRange(IEnumerable<Vehicle> vehiclesToReturn) =>
+        Setup(cr => cr.GetRangeAsync(It.IsAny<int>(), It.IsAny<int>(), _anyToken))
+            .ReturnsAsync(vehiclesToReturn);
+
+    public void GetById(Vehicle vehicleToReturn) =>
+        Setup(cr => cr.GetByIdAsync(It.IsAny<Guid>(), _anyToken))
+            .ReturnsAsync(vehicleToReturn);
+
+    public void IsExists(bool boolToReturn) =>
+        Setup(cr => cr.IsExistsAsync(It.IsAny<Expression<Func<Vehicle, bool>>>(), _anyToken))
+        .ReturnsAsync(boolToReturn);
+}

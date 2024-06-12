a7f608eebd43636e6ee15cb176d733b56f238142
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-12 11:39:14 +0300

    feat: add generic repo interface

---
+++ DAL/Repositories/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace DAL.Repositories.Interfaces;
+
+public interface IRepositoryBase<TEntity> where TEntity : class
+{
+    Task<IQueryable<TEntity>> GetRangeAsync(int page, int pageSize, CancellationToken cancellationToken);
+
+    Task<IQueryable<TEntity>> GetByConditionAsync(Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken);
+
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
+
+    Task RemoveAsync(TEntity entity, CancellationToken cancellationToken);
+}

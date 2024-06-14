cf0bf2eb9c9523b5162ee8e16fdf39a10311c161
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-14 11:39:29 +0300

    Feature/add unit tests for services (#60)
    * feat: add test project to solution
    
    * feat: implement repos mocks
    
    * feat: add data generation with Bogus
    
    * feat: add ClientServiceTests
    
    * refactor: naming violation
    
    * feat: add basic unit testing logic

---
+++ Tests/Mocks/ManufacturerRepositoryMock.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using Moq;
+using System.Linq.Expressions;
+
+namespace Tests.Mocks;
+
+internal class ManufacturerRepositoryMock : Mock<IManufacturerRepository>
+{
+    private readonly CancellationToken _anyToken = It.IsAny<CancellationToken>();
+
+    public void GetRange(IEnumerable<Manufacturer> manufacturersToReturn) =>
+        Setup(cr => cr.GetRangeAsync(It.IsAny<int>(), It.IsAny<int>(), _anyToken))
+            .ReturnsAsync(manufacturersToReturn);
+
+    public void GetById(Manufacturer manufacturerToReturn) =>
+        Setup(cr => cr.GetByIdAsync(It.IsAny<Guid>(), _anyToken))
+            .ReturnsAsync(manufacturerToReturn);
+
+    public void IsExists(bool boolToReturn) =>
+        Setup(cr => cr.IsExistsAsync(It.IsAny<Expression<Func<Manufacturer, bool>>>(), _anyToken))
+        .ReturnsAsync(boolToReturn);
+}

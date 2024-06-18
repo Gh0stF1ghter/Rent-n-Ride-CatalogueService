c56eb36919be9832b92cacf3f9edf5401969577f
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-18 17:16:16 +0300

    feat: add caching extensions

---
+++ DAL/Extensions/Cache.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace DAL.Extensions;
+internal static class Cache
+{
+    public static T GetDataFromCache<T>(this byte[] cache)
+    {
+        var serializedData = Encoding.UTF8.GetString(cache);
+        var data = JsonConvert.DeserializeObject<T>(serializedData)!;
+
+        return data;
+    }
+
+    public static (byte[], DistributedCacheEntryOptions) ConvertDataForCaching<T>(this T data)
+    {
+        var serializedData = JsonConvert.SerializeObject(data);
+
+        var bytedData = Encoding.UTF8.GetBytes(serializedData);
+
+        var options = new DistributedCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromMinutes(2))
+            .SetAbsoluteExpiration(DateTime.Now.AddMinutes(10));
+
+        return (bytedData, options);
+    }
+}

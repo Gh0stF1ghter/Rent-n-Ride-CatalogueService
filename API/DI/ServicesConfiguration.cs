5803bd89fd3b29f6b5df2215fc18f9b7b7dda05d
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-18 12:42:37 +0300

    feat: add jwt validation

--- API/DI/ServicesConfiguration.cs
+++ API/DI/ServicesConfiguration.cs
@@ -1,14 +1,21 @@
-﻿using FluentValidation;
+﻿using API.Extensions;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 using System.Reflection;
+using System.Text;
 
 namespace API.DI;
 
 public static class ServicesConfiguration
 {
-    public static void AddApiDependencies(this IServiceCollection services)
+    public static void AddApiDependencies(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddAutoValidation();
+        services.AddAuthentication(configuration);
+
+        services.ConfigureSwagger();
     }
 
     private static void AddAutoValidation(this IServiceCollection services)
@@ -16,4 +23,23 @@ public static class ServicesConfiguration
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddFluentValidationAutoValidation();
     }
+
+    private static void AddAuthentication(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
 }

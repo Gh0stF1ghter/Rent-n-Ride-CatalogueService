126a3da3db45d1643377e64c67cd30a40f7861eb
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-24 12:20:26 +0300

    feat: add Catalog db context

---
+++ Catalog.DataAccess/Context/CatalogDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.DataAccess.Context;
+
+public class CatalogDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbSet<ManufacturerEntity> Manufacturers { get; set; }
+    public DbSet<CarModelEntity> VehicleModels { get; set; }
+    public DbSet<VehicleEntity> Vehicles { get; set; }
+
+}
---
+++ Catalog.DataAccess/DI/ServicesConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DAL.DI;
+
+public static class ServicesConfiguration
+{
+    public static void AddDataAccessDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<CatalogDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DBConnection")));
+    }
+}

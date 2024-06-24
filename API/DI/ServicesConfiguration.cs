894b5f1c30139a8adb95513f75dfb36427d4414a
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-24 18:14:10 +0300

    feat: add authentication via auth0

--- API/DI/ServicesConfiguration.cs
+++ API/DI/ServicesConfiguration.cs
@@ -1,14 +1,29 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 using System.Reflection;
+using System.Security.Claims;
 
 namespace API.DI;
 
 public static class ServicesConfiguration
 {
-    public static void AddApiDependencies(this IServiceCollection services)
+    public static void AddApiDependencies(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddAutoValidation();
+
+        services.AddAuthentication(
+            options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(
+            options =>
+            {
+                options.Authority = configuration["Auth0:Domain"];
+                options.Audience = configuration["Auth0:Audience"];
+            });
     }
 
     private static void AddAutoValidation(this IServiceCollection services)

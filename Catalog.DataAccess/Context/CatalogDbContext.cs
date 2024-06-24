358545d8f5ea839d0cd0a78f21063162dca77d30
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-24 13:04:13 +0300

    feat: add data generator

--- Catalog.DataAccess/Context/CatalogDbContext.cs
+++ Catalog.DataAccess/Context/CatalogDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Catalog.DataAccess.Entities;
+using DAL.Context;
 using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.DataAccess.Context;
@@ -6,7 +7,17 @@ namespace Catalog.DataAccess.Context;
 public class CatalogDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
 {
     public DbSet<ManufacturerEntity> Manufacturers { get; set; }
-    public DbSet<CarModelEntity> VehicleModels { get; set; }
+    public DbSet<CarModelEntity> CarModelEntities { get; set; }
     public DbSet<VehicleEntity> Vehicles { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        DataGenerator.Init();
+
+        modelBuilder.Entity<ManufacturerEntity>().HasData(DataGenerator.Manufacturers);
+        modelBuilder.Entity<CarModelEntity>().HasData(DataGenerator.CarModelEntities);
+        modelBuilder.Entity<VehicleEntity>().HasData(DataGenerator.Vehicles);
+    }
 }
---
+++ Catalog.DataAccess/Context/DataGenerator.cs
@@ -0,0 +1,81 @@
+using Bogus;
+using Catalog.DataAccess.Entities;
+using Catalog.DataAccess.Enums;
+
+namespace DAL.Context;
+
+internal static class DataGenerator
+{
+    private const int CountOfGeneratedUnits = 5;
+
+    public static readonly List<ManufacturerEntity> Manufacturers = [];
+    public static readonly List<CarModelEntity> CarModelEntities = [];
+    public static readonly List<VehicleEntity> Vehicles = [];
+
+    public static void Init()
+    {
+        if (Manufacturers.Count > 0)
+            return;
+
+        AddManufacturerData();
+    }
+
+    private static void AddManufacturerData()
+    {
+        var manufacturerFaker = GetManufacturerFaker();
+
+        var generatedManufacturers = manufacturerFaker.Generate(CountOfGeneratedUnits);
+
+        Manufacturers.AddRange(generatedManufacturers);
+    }
+
+    private static void AddModelData(Guid manufacturerId)
+    {
+        var modelFaker = GetModelFaker(manufacturerId);
+
+        var generatedModels = modelFaker.Generate(CountOfGeneratedUnits);
+
+        CarModelEntities.AddRange(generatedModels);
+    }
+
+    private static void AddVehicleData(Guid modelId)
+    {
+        var vehicleFaker = GetVehicleFaker(modelId);
+
+        var generatedVehicles = vehicleFaker.Generate(CountOfGeneratedUnits);
+
+        Vehicles.AddRange(generatedVehicles);
+    }
+
+    private static Faker<ManufacturerEntity> GetManufacturerFaker() =>
+        new Faker<ManufacturerEntity>()
+            .RuleFor(m => m.Id, _ => Guid.NewGuid())
+            .RuleFor(m => m.Name, f => f.Vehicle.Manufacturer())
+            .RuleFor(m => m.ModelNames, (_, m) =>
+            {
+                AddModelData(m.Id);
+                return [];
+            });
+
+    private static Faker<CarModelEntity> GetModelFaker(Guid manufacturerId) =>
+        new Faker<CarModelEntity>()
+            .RuleFor(m => m.Id, _ => Guid.NewGuid())
+            .RuleFor(m => m.Name, f => f.Vehicle.Model())
+            .RuleFor(m => m.ManufacturerId, _ => manufacturerId)
+            .RuleFor(m => m.Vehicles, (_, m) =>
+            {
+                AddVehicleData(m.Id);
+                return [];
+            });
+
+    private static Faker<VehicleEntity> GetVehicleFaker(Guid modelId) =>
+        new Faker<VehicleEntity>()
+            .RuleFor(v => v.Id, _ => Guid.NewGuid())
+            .RuleFor(v => v.PlateNumber, f => f.Vehicle.Vin())
+            .RuleFor(v => v.Odo, f => f.Random.Number(0, 10000))
+            .RuleFor(v => v.RentCost, f => f.Random.Double(100.0, 4000.9))
+            .RuleFor(v => v.ModelId, _ => modelId)
+            .RuleFor(v => v.VehicleType, f => f.PickRandom<VehicleType>())
+            .RuleFor(v => v.VehicleState, f => f.PickRandom<VehicleState>())
+            .RuleFor(v => v.FuelType, f => f.PickRandom<FuelType>());
+}

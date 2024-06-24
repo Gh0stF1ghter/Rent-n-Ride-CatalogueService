d5bfded3a202d5640560d6eb5ff9123f4c8f711a
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-24 13:17:05 +0300

    refactor: sync namespaces

--- User.DataAccess/Context/AgencyDbContext.cs
+++ User.DataAccess/Context/AgencyDbContext.cs
@@ -1,9 +1,18 @@
-using DAL.Entities;
 using Microsoft.EntityFrameworkCore;
+using User.DataAccess.Entities;
 
-namespace DAL.Context;
+namespace User.DataAccess.Context;
 
 public class UsersDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
 {
     public DbSet<UserEntity> Clients { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        DataGenerator.Init();
+
+        modelBuilder.Entity<UserEntity>().HasData(DataGenerator.Clients);
+    }
 }
--- User.DataAccess/Entities/UserEntity.cs
+++ User.DataAccess/Entities/UserEntity.cs
@@ -1,4 +1,4 @@
-﻿namespace DAL.Entities;
+﻿namespace User.DataAccess.Entities;
 
 public class UserEntity
 {

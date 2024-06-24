612a075a7ea706b9c75cedc1113b10cdb8c6d487
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-24 12:47:55 +0300

    feat: add user entity

---
+++ User.DataAccess/Entities/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL.Entities;
+
+public class UserEntity
+{
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}

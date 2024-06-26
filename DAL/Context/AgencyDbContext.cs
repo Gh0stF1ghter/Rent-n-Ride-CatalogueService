e07c3a6d40b9cfcab5583fcad32645ab69eef755
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-26 16:03:53 +0300

    Feature/rent add dal (#80)
    * feat: add entities
    
    * feat: add data generator
    
    * feat: add db context
    
    * feat: move rent to separate repository
    
    * fix: remove redutant project

--- DAL/Context/AgencyDbContext.cs
+++ DAL/Context/AgencyDbContext.cs
@@ -10,17 +10,4 @@ public class AgencyDbContext(DbContextOptions dbContextOptions) : DbContext(dbCo
     public DbSet<CarModelEntity> VehicleModels { get; set; }
     public DbSet<VehicleEntity> Vehicles { get; set; }
     public DbSet<VehicleClientHistoryEntity> VehicleClientHistories { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        DataGenerator.Init();
-
-        modelBuilder.Entity<ManufacturerEntity>().HasData(DataGenerator.Manufacturers);
-        modelBuilder.Entity<CarModelEntity>().HasData(DataGenerator.VehicleModels);
-        modelBuilder.Entity<VehicleEntity>().HasData(DataGenerator.Vehicles);
-        modelBuilder.Entity<ClientEntity>().HasData(DataGenerator.Clients);
-        modelBuilder.Entity<VehicleClientHistoryEntity>().HasData(DataGenerator.VehicleClientHistories);
-    }
 }

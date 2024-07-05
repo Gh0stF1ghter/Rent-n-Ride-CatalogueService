f6be729302253ed3e8ebf03380c7bda30a76cbe8
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-05 14:41:26 +0300

    feat: add UpdateVehicleViewModel

---
+++ API/ViewModels/UpdateViewModels/UpdateVehicleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Enums;
+
+namespace API.ViewModels.UpdateViewModels;
+
+public record UpdateVehicleViewModel(
+    string PlateNumber,
+    decimal RentCost,
+    int Odo,
+    bool IsRented,
+    VehicleType VehicleType = VehicleType.None,
+    VehicleState VehicleState = VehicleState.None,
+    FuelType FuelType = FuelType.None
+);

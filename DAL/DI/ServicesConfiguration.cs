b71f083e1cb35b4a0db37b5105085f69d8fe8be3
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-09 14:47:49 +0300

    fix: move sensitive data to .env file

--- DAL/DI/ServicesConfiguration.cs
+++ DAL/DI/ServicesConfiguration.cs
@@ -11,7 +11,9 @@ public static class ServicesConfiguration
 {
     public static void AddDataAccessDependencies(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<CatalogueDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DBConnection")));
+        var connection = configuration.GetConnectionString("DBConnection") + Environment.GetEnvironmentVariable("POSTGRES_USER") + Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+        services.AddDbContext<CatalogueDbContext>(options => options.UseNpgsql(connection));
 
         services.AddTransient<IManufacturerRepository, ManufacturerRepository>();
         services.AddTransient<ICarModelRepository, CarModelRepository>();

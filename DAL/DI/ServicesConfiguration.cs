94d932e08c96178dc13a8a55e8daf8efdb78c3c4
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-11 14:10:47 +0300

    Feature/implement docker and docker compose (#85)
    * feat: create docker orchestrator & adjust connections
    
    * fix: remove auto migration from onModelCreating
    
    * refactor: use swagger on every environment
    
    * feat: adjust connection string
    
    * feat: add dependencies to containers
    
    * fix: move docker compose to separate folder
    
    * Update sonarcloud.yml
    
    * fix: move sensitive data to .env file
    
    * feat: update connection string
    
    * Update sonarcloud.yml

--- DAL/DI/ServicesConfiguration.cs
+++ DAL/DI/ServicesConfiguration.cs
@@ -11,7 +11,7 @@ public static class ServicesConfiguration
 {
     public static void AddDataAccessDependencies(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<CatalogueDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DBConnection")));
+        services.AddDbContext<CatalogueDbContext>(options => options.UseNpgsql($"{configuration.GetConnectionString("DBConnection")} User ID={Environment.GetEnvironmentVariable("POSTGRES_USER")}; Password={Environment.GetEnvironmentVariable("POSTGRES_PASSWORD")}"));
 
         services.AddTransient<IManufacturerRepository, ManufacturerRepository>();
         services.AddTransient<ICarModelRepository, CarModelRepository>();

5e4c54b86aba077a64a29a6325813bc5524b4f30
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-16 13:18:36 +0300

    feat: add carmodel consumers

---
+++ BLL/Consumers/CarModelConsumers/CarModelCreatedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Models;
+using BLL.Services.Interfaces;
+using EventBus.CatalogueServiceEvents.CarModelEvents;
+using Mapster;
+using MassTransit;
+
+namespace BLL.Consumers.CarModelConsumers;
+
+public class CarModelCreatedConsumer(ICarModelService service) : IConsumer<CarModelCreated>
+{
+    public async Task Consume(ConsumeContext<CarModelCreated> context)
+    {
+        var carModelFromEvent = context.Message;
+
+        await Console.Out.WriteLineAsync($"car model {carModelFromEvent.Name} consumed to create");
+
+        var carModel = carModelFromEvent.Adapt<CarModel>();
+
+        await service.AddAsync(carModel, default);
+    }
+}
---
+++ BLL/Consumers/CarModelConsumers/CarModelUpdatedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Models;
+using BLL.Services.Interfaces;
+using EventBus.CatalogueServiceEvents.CarModelEvents;
+using Mapster;
+using MassTransit;
+
+namespace BLL.Consumers.CarModelConsumers;
+
+public class CarModelUpdatedConsumer(ICarModelService service) : IConsumer<CarModelUpdated>
+{
+    public async Task Consume(ConsumeContext<CarModelUpdated> context)
+    {
+        var carModelFromEvent = context.Message;
+
+        await Console.Out.WriteLineAsync($"car model {carModelFromEvent.Name} consumed to update");
+
+        var carModel = carModelFromEvent.Adapt<CarModel>();
+
+        await service.UpdateAsync(carModel, default);
+    }
+}

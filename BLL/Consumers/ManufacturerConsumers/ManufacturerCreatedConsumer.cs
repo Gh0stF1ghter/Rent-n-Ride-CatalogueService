ccda931ab73cb575bc210d0510a947e9839e320c
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-16 13:18:49 +0300

    feat: add manufacturer consumers

---
+++ BLL/Consumers/ManufacturerConsumers/ManufacturerCreatedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Models;
+using BLL.Services.Interfaces;
+using EventBus.CatalogueServiceEvents.ManufacturerEvents;
+using Mapster;
+using MassTransit;
+
+namespace BLL.Consumers.ManufacturerConsumers;
+
+public class ManufacturerCreatedConsumer(IManufacturerService service) : IConsumer<ManufacturerCreated>
+{
+    public async Task Consume(ConsumeContext<ManufacturerCreated> context)
+    {
+        var manufacturerFromEvent = context.Message;
+
+        await Console.Out.WriteLineAsync($"manufacturer {manufacturerFromEvent.Name} consumed to create");
+
+        var manufacturer = manufacturerFromEvent.Adapt<ManufacturerModel>();
+
+        await service.AddAsync(manufacturer, default);
+    }
+}
---
+++ BLL/Consumers/ManufacturerConsumers/ManufacturerDeletedConsumer.cs
@@ -0,0 +1,17 @@
+﻿using BLL.Services.Interfaces;
+using EventBus.CatalogueServiceEvents.ManufacturerEvents;
+using MassTransit;
+
+namespace BLL.Consumers.ManufacturerConsumers;
+
+public class ManufacturerDeletedConsumer(IManufacturerService service) : IConsumer<ManufacturerDeleted>
+{
+    public async Task Consume(ConsumeContext<ManufacturerDeleted> context)
+    {
+        var manufacturerId = context.Message.Id;
+
+        await Console.Out.WriteLineAsync($"manufacturer {manufacturerId} consumed to delete");
+
+        await service.DeleteAsync(manufacturerId, default);
+    }
+}

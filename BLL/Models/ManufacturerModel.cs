7d7003a7658007c92aaf9c9a72bb2bed8f9cb0a8
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-29 18:23:25 +0300

    fix: dto mapping issues

--- BLL/Models/ManufacturerModel.cs
+++ BLL/Models/ManufacturerModel.cs
@@ -5,5 +5,5 @@ public class ManufacturerModel
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
 
-    public IEnumerable<CarModel>? ModelNames { get; set; }
+    public IEnumerable<CarModel>? CarModels { get; set; }
 }
